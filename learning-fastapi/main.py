eed786f9c03f371caa6ff9f1bc3e9e548bc9bef1
ASK-Baloch <balochsaeed888@gmail.com>
2024-02-27 20:22:44 +0500

    functions in fastapi

--- learning-fastapi/main.py
+++ learning-fastapi/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlmodel import SQLModel, Field, create_engine, Session, select
-import uvicorn
 
 
 app = FastAPI()
@@ -8,11 +7,15 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'data': {
-        'name': 'Ahmed'
-    }}
+    return {'data': 'blog list'}
 
 
-@app.get('/about')
-def about():
-    return {'data': 'about title'}
+@app.get('/blog/{id}')
+def abc(id):
+    return {'Blog No': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': f'blog comments: {id}'}
+    # return {'data': {'1', '2'}}

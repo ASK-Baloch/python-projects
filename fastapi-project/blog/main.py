2d5dc91018467ff01bd11db003917ae21b73e2b1
ASK-Baloch <balochsaeed888@gmail.com>
2024-02-29 18:27:37 +0500

    creating relations with db

--- fastapi-project/blog/main.py
+++ fastapi-project/blog/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-from schemas import Blog
-
+from sqlmodel import SQLModel, Field, create_engine, Session, select
+# from . import schemas
+from . import models
+from database import engine
 
 app = FastAPI()
+
+models.Blog.metadata.create_all(engine)
+
+class Blog(SQLModel):
+    title: str
+    body: str
+    # published: bool
 
 
 @app.post('/blog')

be8b07bd7b4d9dc2f0dd488d2029d6a12ae93c1a
ASK-Baloch <balochsaeed888@gmail.com>
2024-03-01 17:46:25 +0500

    starting again

--- fastapi-project/blog/main.py
+++ fastapi-project/blog/main.py
@@ -1,19 +1,26 @@
 from fastapi import FastAPI
-from sqlmodel import SQLModel, Field, create_engine, Session, select
-from .schemas import Blog
-from .models import Base
-from database import engine
+from sqlmodel import SQLModel
 
-app = FastAPI()
+# from sqlmodel import SQLModel, Field, create_engine, Session, select ,Column
+# from database import engine
 
-Base.metadata.create_all(engine)
+
+# class Base(SQLModel , table = True):
+#     id= Column(int , primary_key= True, index = True)
+#     title = Column(str)
+#     body = Column(str)
+
 
 # class Blog(SQLModel):
 #     title: str
 #     body: str
 #     # published: bool
 
+# Base.metadata.create_all(engine)
+
+app = FastAPI()
+
 
 @app.post('/blog')
-def create(req: Blog):
+def create(req):
     return req

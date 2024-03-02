bfde0dcd0b35fd5f034c44edc382c5fcb3a60ea1
ASK-Baloch <balochsaeed888@gmail.com>
2024-03-02 22:10:25 +0500

    code upgraded to sqlmodal .

--- fastapi-project/blog/main.py
+++ fastapi-project/blog/main.py
@@ -1,37 +1,39 @@
 from fastapi import FastAPI
-from sqlmodel import SQLModel
-
 from sqlmodel import SQLModel, Field, create_engine, Session, select, Column
-# from database import engine
 from dotenv import load_dotenv, find_dotenv
 from os import getenv
 
-from typing import Annotated
+# Load environment variables
+load_dotenv(find_dotenv())
 
-_: bool = load_dotenv(find_dotenv())
-
-
+# Get the PostgreSQL URL from environment variables
 postgres_url: str = getenv("POSTGRESQL_URL")
+
+# Create the SQLModel engine
 engine = create_engine(postgres_url)
+
+# Define the base model
 
 
 class Base(SQLModel, table=True):
-    id = Column(int, primary_key=True, index=True)
-    title = Column(str)
-    body = Column(str)
+    id:int = Field(int, primary_key=True, index=True)
+    title:str = Field(str)
+    body:str = Field(str)
+
+# Define the Blog model
 
 
-class Blog(SQLModel):
-    title: str
-    body: str
-    # published: bool
+class Blog(Base):
+    pass
 
 
+# Create all tables
 Base.metadata.create_all(engine)
 
+# Create the FastAPI application
 app = FastAPI()
 
 
 @app.post('/blog')
-def create(req):
+def create(req: Blog):
     return req

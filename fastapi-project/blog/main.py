be21487b5f815c216aa3eda043180dff70779955
ASK-Baloch <balochsaeed888@gmail.com>
2024-03-02 21:58:05 +0500

    last fix

--- fastapi-project/blog/main.py
+++ fastapi-project/blog/main.py
@@ -1,22 +1,33 @@
 from fastapi import FastAPI
 from sqlmodel import SQLModel
 
-# from sqlmodel import SQLModel, Field, create_engine, Session, select ,Column
+from sqlmodel import SQLModel, Field, create_engine, Session, select, Column
 # from database import engine
+from dotenv import load_dotenv, find_dotenv
+from os import getenv
+
+from typing import Annotated
+
+_: bool = load_dotenv(find_dotenv())
 
 
-# class Base(SQLModel , table = True):
-#     id= Column(int , primary_key= True, index = True)
-#     title = Column(str)
-#     body = Column(str)
+postgres_url: str = getenv("POSTGRESQL_URL")
+engine = create_engine(postgres_url)
 
 
-# class Blog(SQLModel):
-#     title: str
-#     body: str
-#     # published: bool
+class Base(SQLModel, table=True):
+    id = Column(int, primary_key=True, index=True)
+    title = Column(str)
+    body = Column(str)
 
-# Base.metadata.create_all(engine)
+
+class Blog(SQLModel):
+    title: str
+    body: str
+    # published: bool
+
+
+Base.metadata.create_all(engine)
 
 app = FastAPI()
 

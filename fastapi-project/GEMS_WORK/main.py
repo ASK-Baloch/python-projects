a6ac532b711caa70ead29abbf83d3fe9ee5c42e9
ASK-Baloch <balochsaeed888@gmail.com>
2024-03-13 19:43:23 +0500

    commiting if any bugs comes up

--- fastapi-project/GEMS_WORK/main.py
+++ fastapi-project/GEMS_WORK/main.py
@@ -1,18 +1,19 @@
 from fastapi import FastAPI
 import uvicorn
 from database import engine
-from sqlmodel import create_engine,SQLModel
+from sqlmodel import create_engine, SQLModel
 
 app = FastAPI()
+
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
-
 @app.get('/')
 def hello():
     return 'Hello, world!'
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='localhost', port=8000)

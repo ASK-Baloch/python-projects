9157bd42ae9b972ba7f2f9be8f573fdc5e59cdcb
ASK-Baloch <balochsaeed888@gmail.com>
2024-03-13 11:43:18 +0500

    class made for Geo Colors

--- fastapi-project/GEMS_WORK/models/gem_model.py
+++ fastapi-project/GEMS_WORK/models/gem_model.py
@@ -2,16 +2,28 @@ from sqlmodel import SQLModel, Field
 from enum import Enum
 from typing import Optional
 
+
 class GeoClarity(Enum):
     FL = "FL"
     VS = 'VS'
     WS = 'WS'
     SI = 'SI'
 
+
+class GemColor(Enum):
+    D = "D"
+    E = "E"
+    G = "G"
+    F = "F"
+    H = "H"
+    I = "I"
+
+
 class Gem(SQLModel, table=True):
-    id: int = Field(primary_key=True,index=True)
-    size:float = 1
+    id: int = Field(primary_key=True, index=True)
+    size: float = 1
     name: str
     description: str
     price: float
-    clarity:Optional[GeoClarity]
+    clarity: Optional[GeoClarity] = None
+    color: Optional[GemColor] = None

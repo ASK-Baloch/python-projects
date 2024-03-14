0f9b639f1bc13ec9865137b41d3a796a4ed054f0
ASK-Baloch <balochsaeed888@gmail.com>
2024-03-14 22:27:57 +0500

    writing comments in GEMS_WORK.main.py

--- fastapi-project/GEMS_WORK/main.py
+++ fastapi-project/GEMS_WORK/main.py
@@ -6,9 +6,13 @@ from models.gem_model import *
 
 app = FastAPI()
 
+# this function create tables in database and bring models into tables
+
 
 def create_db_and_tables():
     Gem.metadata.create_all(engine)
+
+# this is a route that is a get method that returns a string upon calling
 
 
 @app.get('/')
@@ -16,6 +20,7 @@ def hello():
     return 'Hello, world!'
 
 
+# this is a condition that if the file name is main. then run server and call the functin relatable to creating tables
 if __name__ == '__main__':
     uvicorn.run(app, host='localhost', port=8000)
     create_db_and_tables()

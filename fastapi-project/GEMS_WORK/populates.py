265005fa655ab1015b2a79f2bea217a79d76b675
ASK-Baloch <balochsaeed888@gmail.com>
2024-03-15 22:16:33 +0500

    writing comments in code

--- fastapi-project/GEMS_WORK/populates.py
+++ fastapi-project/GEMS_WORK/populates.py
@@ -2,18 +2,18 @@ from models.gem_model import Gem, GemProperties
 import random
 import string
 color_grades=string.ascii_uppercase[3:9]
-
+# creating gem props to further use like an interface
 def create_gem_props():
     size = random.randInt(3, 70)/10
     color = color_grades(random.randint(0,5))
     clarity = random.randint(1,4)
     gem_p=GemProperties(size=size,color=color,clarity=clarity)
     return gem_p
-
+# funtion that will be used to create a gem
 def create_gem():
     gem = Gem(price=1000,gem_properties_id=gem_p)
     return gem
-
+# funtion that will be used to create tables in database
 def create_gems_db():
     # for i in range(10):
     #     gem_p = create_gem_props()

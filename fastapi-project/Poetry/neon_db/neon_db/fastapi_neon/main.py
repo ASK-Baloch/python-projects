908055bd035a29f9d55a78bcfcc15a63c676ae78
ASK-Baloch <balochsaeed888@gmail.com>
2024-03-17 22:57:24 +0500

    directly using the db_url in main.py[not a good practice will change later]

--- fastapi-project/Poetry/neon_db/neon_db/fastapi_neon/main.py
+++ fastapi-project/Poetry/neon_db/neon_db/fastapi_neon/main.py
@@ -7,14 +7,14 @@ from fastapi import FastAPI, Depends
 from starlette.config import Config
 from starlette.datastructures import Secret
 
-try:
-    config = Config(".env")
-except FileNotFoundError:
-    config = Config()
+# try:
+#     config = Config(".env")
+# except FileNotFoundError:
+#     config = Config()
 
-DATABASE_URL = config("DATABASE_URL", cast=Secret)
-
-TEST_DATABASE_URL = config("TEST_DATABASE_URL", cast=Secret)
+# DATABASE_URL = config("DATABASE_URL", cast=Secret)
+# TEST_DATABASE_URL = config("TEST_DATABASE_URL", cast=Secret)
+DATABASE_URL = "postgresql://ASK-Baloch:nguHM17PVNEz@ep-falling-surf-a5h154g7.us-east-2.aws.neon.tech/neondb?sslmode=require"
 
 
 class Todo(SQLModel, table=True):

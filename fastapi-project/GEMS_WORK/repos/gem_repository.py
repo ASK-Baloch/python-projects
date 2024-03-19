29211b08d958600308e255d8884becefd70ccb01
ASK-Baloch <balochsaeed888@gmail.com>
2024-03-19 21:54:54 +0500

    idk what tf isit

--- fastapi-project/GEMS_WORK/repos/gem_repository.py
+++ fastapi-project/GEMS_WORK/repos/gem_repository.py
@@ -1,13 +1,17 @@
-from main import engine
+from database import engine
 from sqlmodel import SQLModel, Session, select
 from models.gem_model import Gem, GemProperties
+import uvicorn
+from main import create_db_and_tables ,app
+
 
 
 def select_gem():
-    with Session as session:
+    with Session(engine) as session:
         statement = select(Gem)
         result = session.exec(statement)
         print(result.all())
 
 
 select_gem()
+

166e430cce5539bed7d9234168d209a2dc50b193
ASK-Baloch <balochsaeed888@gmail.com>
2024-03-26 20:36:12 +0500

    installed some modules

--- fastapi-project/COURSE_PROJECT/scripts.py
+++ fastapi-project/COURSE_PROJECT/scripts.py
@@ -1,23 +1,24 @@
-from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, select
+from sqlmodel import SQLModel, Field, create_engine, Session
 from typing import List, Optional
 from dotenv import load_dotenv, find_dotenv
 from os import getenv
 import json
 
-_: bool = load_dotenv(find_dotenv())
+
+class Rating(SQLModel, table=False):
+    total: int
+    count: int
 
 
-postgres_url: str = getenv("DATABASE_URL")  # type: ignore
-
-
-class Course(SQLModel):
+class Course(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     name: str = Field(index=True, unique=True, nullable=False)
-    chapters: str = Field(default="", sa_column_kwargs={"type": "JSONB"})
-    rating: dict = Field(default={"total": 0, "count": 0})
+    chapters: str = Field(default="[]", sa_column_kwargs={"type": "JSONB"})
+    rating: Rating = Field(default=Rating(total=0, count=0))
 
 
-# Connect to PostgreSQL database
+_: bool = load_dotenv(find_dotenv())
+postgres_url: str = getenv("DATABASE_URL")  # type: ignore
 engine = create_engine(postgres_url, echo=True)
 
 
@@ -31,13 +32,15 @@ with open("courses.json", "r") as f:
 
 # Add rating field to each course and chapter
 for course in courses:
-    course["chapters"] = [{"rating": {"total": 0, "count": 0}} for _ in course["chapters"]]
+    course["chapters"] = [{"rating": {"total": 0, "count": 0}}
+                          for _ in course["chapters"]]
     course["rating"] = {"total": 0, "count": 0}
 
 # Insert courses into table
 with Session(engine) as session:
     for course in courses:
-        course_obj = Course(name=course["name"], chapters=json.dumps(course["chapters"]))
+        course_obj = Course(name=course["name"],
+                            chapters=json.dumps(course["chapters"]))
         session.add(course_obj)
     session.commit()
 
@@ -45,3 +48,6 @@ with Session(engine) as session:
     session.commit()
 
 print("Courses data successfully imported into PostgreSQL.")
+
+if __name__ == "__main__":
+    create_db_and_tables()

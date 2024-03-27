90f888dacae5a7f236431d39f861b6dd836e11b3
ASK-Baloch <balochsaeed888@gmail.com>
2024-03-27 22:19:19 +0500

    added comments in fastapi1/main

--- fastapi-project/Poetry/fastapi/fastapi1/main.py
+++ fastapi-project/Poetry/fastapi/fastapi1/main.py
@@ -10,10 +10,16 @@ class Todo(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     content: str = Field(index=True)
 
+
+# only needed for psycopg 3 - replace postgresql
+# with postgresql+psycopg in settings.DATABASE_URL
 connection_string = str(settings.DATABASE_URL).replace(
     "postgresql", "postgresql+psycopg"
 )
 
+
+# recycle connections after 5 minutes
+# to correspond with the compute scale down
 engine = create_engine(
     connection_string, connect_args={"sslmode": "require"}, pool_recycle=300
 )
@@ -22,6 +28,10 @@ engine = create_engine(
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
+
+# The first part of the function, before the yield, will
+# be executed before the application starts.
+# https://fastapi.tiangolo.com/advanced/events/#lifespan-function
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Creating tables..")

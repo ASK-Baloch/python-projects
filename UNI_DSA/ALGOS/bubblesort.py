dab5df703221af2c7487b719b03a214724869afc
ASK-Baloch <balochsaeed888@gmail.com>
2024-03-30 23:35:26 +0500

    writing bubble sort algo

---
+++ UNI_DSA/ALGOS/bubblesort.py
@@ -0,0 +1,6 @@
+#                                           Bubble Sort Algorithm
+# bubbleSort(array)
+#   for i <- 1 to indexOfLastUnsortedElement-1
+#     if leftElement > rightElement
+#       swap leftElement and rightElement
+# end bubbleSort
